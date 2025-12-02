922d068f66a055182e7171b4fb14d94aa3b39ce5
Aidan <pianomanaidan@gmail.com>
2025-12-02 14:34:05 -0500

    Set colours

--- drainage_profiles.m
+++ drainage_profiles.m
@@ -30,7 +30,13 @@ function drainage_profiles()
     options = odeset('RelTol', 1e-5, 'AbsTol', 1e-6, 'Events', @detect_empty, 'MaxStep', 0.01);
     figure(1); clf; hold on;
     set(gcf, 'Color', "white", "Name", "Drainage Profiles");
-    colors = lines(length(L_values));
+    colors = [0 0.4470 0.7410;
+             0.8500 0.3250 0.0980;
+             0.9290 0.6940 0.1250;
+             0.4940 0.1840 0.5560;
+             0.4660 0.6740 0.1880;
+             0.3010 0.7450 0.9330;
+             0.6350 0.0780 0.1840];
     
     fprintf('Tube Length (m) | Total Time (s)\n');
     fprintf('--------------------------------\n');
