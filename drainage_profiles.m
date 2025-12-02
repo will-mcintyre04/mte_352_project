4ebb81eef71a6e7c3614c9eacd24d4023739901a
Aidan Newman <aidannewm@gmail.com>
2025-12-02 16:08:20 -0500

    Revert "added experimental data loader"
    This reverts commit 02449a3df95f3f74a871a1746ac9324fcb5eb8c4.

--- drainage_profiles.m
+++ drainage_profiles.m
@@ -5,7 +5,7 @@ function drainage_profiles()
     % Constants
     g = 9.81;
     rho = 998;
-    mu = 0.001002;
+    mu = 0.001003;
     
     d_tube = 0.00525;
     D = 0.110;
@@ -26,11 +26,17 @@ function drainage_profiles()
     L_values = 0.25:-0.04:0.01;
     
     h0 = 0.14;
-    t_span = [0 120];
-    options = odeset('RelTol', 1e-5, 'AbsTol', 1e-6, 'Events', @detect_empty);
+    t_span = [0 150];
+    options = odeset('RelTol', 1e-5, 'AbsTol', 1e-6, 'Events', @detect_empty, 'MaxStep', 0.01);
     figure(1); clf; hold on;
     set(gcf, 'Color', "white", "Name", "Drainage Profiles");
-    colors = lines(length(L_values));
+    colors = [0 0.4470 0.7410;
+             0.8500 0.3250 0.0980;
+             0.9290 0.6940 0.1250;
+             0.4940 0.1840 0.5560;
+             0.4660 0.6740 0.1880;
+             0.3010 0.7450 0.9330;
+             0.6350 0.0780 0.1840];
     
     fprintf('Tube Length (m) | Total Time (s)\n');
     fprintf('--------------------------------\n');
@@ -46,6 +52,7 @@ function drainage_profiles()
         fprintf('     %.2f       |     %.2f\n', p.L, t(end));
     end
     grid on;
+    xlim(t_span)
     xlabel('Time (seconds)');
     ylabel('Water Height (m)');
     title('Drainage Profiles');
